cf5ed2764e43896ce930d7f4000b74219a309b0a
naetsawd <dechsitbkk@gmail.com>
2023-11-28 18:50:58 -0500

    Added modal text and changed minor styling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "../styles/Header.css";
 
 function Header() {
-	return <div className="header-container"><h1>CV Maker</h1></div>;
+	return <div className="header-container"><h1>CV Planner</h1></div>;
 }
 
 export default Header;
--- src/components/InfoModal.jsx
+++ src/components/InfoModal.jsx
@@ -20,6 +20,18 @@ function InfoModal({ isModalVisible, setIsModalVisible }) {
 					<h2>Information</h2>
 					<img src={addIcon} onClick={closeModal} />
 				</div>
+
+				<ul className="modal-list">
+					<li>This is a resume planner/prototype maker</li>
+					<li>The resume is limited to one page since that is the ideal length</li>
+					<li>
+						You should make sure to provide the right amount of information and
+						include key terms and skills for the target job
+					</li>
+					<li>
+						Use the STAR method when talking about experience and projects
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
